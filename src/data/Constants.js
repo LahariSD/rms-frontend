e46dc9cd5b39ed80a22050e909c1a92a06b9a693
LahariSD <132733743+LahariSD@users.noreply.github.com>
2025-04-25 23:18:26 +0530

    Update Constants.js

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -1,5 +1,5 @@
 export const ROLES = ["admin", "faculty", "student"];
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://rms-backend-4ik5.onrender.com/api";
 export const AUTH_API_ENDPOINTS = {
   LOGIN: "/auth/login",
   REGISTER: "/adduser",
